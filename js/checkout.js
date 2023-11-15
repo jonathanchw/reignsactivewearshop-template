3a04aa6a7fbcd90f2f4b461a23b215b40556419a
jonathanchw <jonathanchw@gmail.com>
2023-11-15 14:29:31 -0300

    adding correct checkout table function

--- js/checkout.js
+++ js/checkout.js
@@ -31,10 +31,37 @@ function showOrderSummary() {
     .join("");
   orderSummary_El.innerHTML = cartHtml + orderSummary_El.innerHTML;
 }
+function handleCheckoutItems() {
+  let cartTable = document.getElementById("checkout-table");
+  cartTable.innerHTML += cartData
+    .map(({ id, name, image, price, shipping }) => {
+      return `
+      <tr id="product-id-${id}" class="data_row product">
+     
+      <td>
+       <img src="../images/products/${image}"
+            class="img-fluid cart-pdt-image" 
+            id="image${id}"
+            alt="${name}"
+          />
+      </td>
+      <td colspan="3" id="singlepdt"><p>${name}</p></td>
+      <td id="singlepdt"><p>One Time Sale</p></td>
+      <td id="price-${id}" class="priceCol">$${price}</td>
+      <td id="shipping-${id}" class="priceCol">$${shipping}</td>
+      <td id="total-${id} class="priceCol">$${(price + shipping).toFixed(
+        2
+      )}</td>
+      </tr>
+      `;
+    })
+    .join("");
+}
 
 updateCartData();
 updateSubTotal();
 showOrderSummary();
+handleCheckoutItems();
 /*********/
 
 /*Handle user-data form*/
